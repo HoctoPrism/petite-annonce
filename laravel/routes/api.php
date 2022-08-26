32d945fabe2385df0a864c4313bf88244ec84f4d
melissa98m <melissa.mangione@gmail.com>
2022-08-26 09:31:29 +0200

    ajout des route denomination et category

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\DenominationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource("categories", CategoryController::class);
+Route::apiResource("denominations", DenominationController::class);
