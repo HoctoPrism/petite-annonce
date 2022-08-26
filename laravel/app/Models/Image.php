b6eb91043e8a97538ca483a2f170fa6ec30a3b5c
jonathan-lescaut <91247137+jonathan-lescaut@users.noreply.github.com>
2022-08-26 17:06:49 +0200

    LARAVEL : load model image

--- laravel/app/Models/Image.php
+++ laravel/app/Models/Image.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Annonce;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 {
     use HasFactory;
     protected $fillable = ['url', 'annonce_id'];
 
-    public function annonces(): BelongsTo
+    public function annonce(): BelongsTo
     {
         return $this->BelongsTo(Annonce::class, 'annonce_id');
     }
