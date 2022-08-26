87c4ca056e4f92caf78ad250632225540f944591
unknown <nemexxix@gmail.com>
2022-08-26 16:35:33 +0200

    LARAVEL : retour des objet complet dans l'api plutôt que l'IRI sur denominations

--- laravel/app/Models/Denomination.php
+++ laravel/app/Models/Denomination.php
@@ -12,6 +12,6 @@ class Denomination extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 }
