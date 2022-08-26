5990f57a3c3db0c3aebdc8c06d9a4affd9a19396
melissa98m <melissa.mangione@gmail.com>
2022-08-26 09:33:43 +0200

    ajout controller  category et denomination

---
+++ laravel/app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = DB::table('categories')
+            ->get()
+            ->toArray();
+        return response()->json(['status' => 'Success', 'data' => $categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name_category' => 'required|max:50',
+        ]);
+        $category = Category::create([
+            'name_category' => $request->name_category,
+        ]);
+        return response()->json(['status' => 'Success', 'data' => $category]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $this->validate($request, [
+            'name_category' => 'required|max:50',
+
+        ]);
+        $category->update([
+            'name_category' => $request->name_category,
+        ]);
+        return response()->json(['status' => 'Mis à jour avec succèss']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(['status' => 'Supprimer avec succès']);
+    }
+}
---
+++ laravel/app/Http/Controllers/API/DenominationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Denomination;
+use Illuminate\Http\Request;
+
+class DenominationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $denominations = Denomination::with(['category'])->get();
+        $denominations->map(function ($denomination){
+            unset($denomination['category_id']);
+        });
+        // On retourne les informations des utilisateurs en JSON
+        return response()->json(['status' => 'Success', 'data' => $denominations]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name_denomination' => 'required|max:100',
+        ]);
+        $denomination = Denomination::create([
+            'category_denomination' => $request->category_denomination,
+            'category_id' => $request->category_id,
+        ]);
+        return response()->json(['status' => 'Success', 'data' => $denomination]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Denomination  $denomination
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Denomination $denomination)
+    {
+        return response()->json($denomination);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Denomination  $denomination
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Denomination $denomination)
+    {
+        $this->validate($request, [
+            'name_denomination' => 'required|max:100',
+
+        ]);
+        $denomination->update([
+            'name_denomination' => $request->name_denomination,
+            'category_id' => $request->category_id,
+        ]);
+        return response()->json(['status' => 'Mis à jour avec succèss']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Denomination  $denomination
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Denomination $denomination)
+    {
+        $denomination->delete();
+        return response()->json(['status' => 'Supprimer avec succès']);
+    }
+}
