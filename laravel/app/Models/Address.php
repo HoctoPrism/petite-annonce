adb7a1d7ba378d083ff9e172074bb93aabc37b89
jonathan-lescaut <91247137+jonathan-lescaut@users.noreply.github.com>
2022-08-26 11:24:13 +0200

    ajout models brand user addresses

---
+++ laravel/app/Models/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+    protected $fillable = ['address', 'postal_code', 'city', 'country', 'phone', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
