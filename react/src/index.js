7585a48a8a0aa3649c9027a4ed607eb5a66c2316
melissa98m <melissa.mangione@gmail.com>
2022-09-01 11:56:45 +0200

    ajout de la route register

--- react/src/index.js
+++ react/src/index.js
@@ -19,8 +19,10 @@ import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import App from './App';
 import Home from "./component/home/home";
 import Type from "./component/type/type";
+import Brand from "./component/brand/brand";
 import {Navbar} from "./component/_partials/_navbar/_navbar";
 import {Footer} from "./component/_partials/_footer/_footer";
+import Register from "./services/auth/register";
 
 function CustomTheme() {
 
@@ -57,6 +59,7 @@ function CustomTheme() {
                 <Routes>
                     <Route exact path="/" element={<RouteService Component={Home}/>}>Accueil</Route>
                     {/*<Route exact path="type" element={<RouteService Component={Type}/>}>Type</Route>*/}
+                    <Route exact path="register" element={<RouteService Component={Register}/>}>Inscription</Route>
                     <Route path="*" element={
                         <div>
                             <p>Il n'y a rien ici !</p>
