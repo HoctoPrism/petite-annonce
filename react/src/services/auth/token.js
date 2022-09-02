9f530c111a3fb9addfbd7f2f311d3afcf2781aa8
HoctoPrism <nemexxix@gmail.com>
2022-09-02 19:06:14 +0200

    REACT : ajout de test dans les fonctions du token

--- react/src/services/auth/token.js
+++ react/src/services/auth/token.js
@@ -5,15 +5,31 @@ let getToken = () => {
 }
 
 let getDecodedToken = () => {
-    return jwtDecode(localStorage.getItem('access_token'));
+    if (getToken()){
+        return jwtDecode(localStorage.getItem('access_token'));
+    } else {
+        return false
+    }
 }
 
 let getRoles = () => {
-    return JSON.parse(getDecodedToken().roles).toString();
+    // On teste si il y a un token décodé et si il n'a pas expiré
+    if (getDecodedToken() && !(getDecodedToken().exp * 1000 < Date.now())){
+        // la valeur de base est un tableau dans un string, on le parse pour faire sauter le string et
+        // on le tostring pour faire sauter le tableau, comme ça on a seulement la valeur
+        return JSON.parse(getDecodedToken().roles).toString();
+    } else {
+        return false
+    }
 }
 
 let getEmail = () => {
-    return getDecodedToken().email;
+    // On teste si il y a un token décodé et si il n'a pas expiré
+    if (getDecodedToken() && !(getDecodedToken().exp * 1000 < Date.now())){
+        return getDecodedToken().email;
+    } else {
+
+    }
 }
 
 export default {getToken, getDecodedToken, getRoles, getEmail}
