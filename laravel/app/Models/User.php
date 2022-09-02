31c1f30b92ca378cf43f58cb7c09eeb1dc4443f8
HoctoPrism <nemexxix@gmail.com>
2022-09-02 17:26:13 +0200

    LARAVEL : ajout des roles et email dans le token afin de pouvoir les utiliser côté front

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -69,6 +69,9 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            "roles" => $this->roles,
+            "email" => $this->email
+        ];
     }
 }
